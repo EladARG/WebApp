332e100aa925ffb006896432e14a4fb96c9588f3
EladARG <elad.argentaro@gmail.com>
2016-03-14 19:29:37 +0200

    stage 2

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,23 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+/**
+ * JS Library v0
+ */
+
+var UTILS = (function () {
+
+    return {
+        /**
+         * Check if a given value is a plain Object
+         *
+         * @param  {*}       o Any value to be checked
+         * @return {Boolean}   true if it's an Object
+         */
+        isObject: function (o) {
+            var toString = Object.prototype.toString;
+            return (toString.call(o) === toString.call({}));
+        }
+    
+    };
+}());
