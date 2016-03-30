0084b85e41c1a0bd56b2639bd44ec4dd3c1fbf63
EladARG <elad.argentaro@gmail.com>
2016-03-31 00:31:55 +0300

    start js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,68 @@
-function Initialize() {
+function Initialize() {//onload
 
 
   
 }
+
+function Search() {//onsearch
+
+
+
+
+}
+function SwitchTab() {//onchange
+
+
+
+
+}
+
+function AdjustDisplay() {
+
+
+
+
+
+}
+
+function ShowSettings(){//onclick
+
+
+
+
+
+}
+
+
+function URLValidation() {
+
+
+
+}
+function SetNameAndURL() {
+
+
+
+}
+// function SetURL() {
+
+
+
+// }
+function Cancel() {//onclick
+
+
+
+}
+function SaveLink() {//onclick
+
+
+
+}
+
+
+
+/*************************************/
+/**********Auxilary Functions*********/
+/*************************************/
+
