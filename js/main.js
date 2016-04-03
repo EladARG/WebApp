d71b24c0bccc4818611b07d1504cd51f2da9e43a
EladARG <elad.argentaro@gmail.com>
2016-04-03 16:09:43 +0300

    fff

--- js/main.js
+++ js/main.js
@@ -356,42 +356,43 @@ window.onhashchange = function(element){
   	else if (prev_tab == "public-folders"){ 
   		child_num =4;
   	}
-  	var curr_child_num=0;
-  	if(elem.id == "quick-reports"){
-  	 	curr_child_num =1;
-	}
-  	else if (elem.id == "my-folders"){
-  	 	curr_child_num =2;
-  	}
-  	else if (elem.id == "my-team-folders"){ 
-  		curr_child_num =3;
-  	}
-  	else if (elem.id == "public-folders"){ 
-  		curr_child_num =4;
-  	}
-	// var list_itemObj = { list_item: li};
-	if(elem!= null){
-		if(elem.id == "quick-reports" || elem.id == "my-folders" ||
-		elem.id == "my-team-folders" || elem.id == "public-folders"){
-		    // OnHashAux(tabs,prev_tab,elem.id,1,li);
-		    setTabStyle(prev_tab, {'display':'none', 'background':'rgb(80, 80, 80)'});
-			if(child_num != 0){
-			    li = tabs.querySelector("ul a:nth-child("+child_num+")");
-			    if(li!=null){
-				    li.style.background = "rgb(80, 80, 80)";
-				    li.style.color =  "white";
-				}
-			}
-			li = tabs.querySelector("ul a:nth-child("+curr_child_num+")");
- 			if(li!=null){
-				li.style.color =  "black";
-				li.style.background = "rgb(235, 235, 235)";
-			}
-			setTabStyle(elem.id , {'display':'block', 'background':'rgb(235, 235, 235)'});
-			Initialize();		
+  	if(elem!= null){
+	  	var curr_child_num=0;
+	  	if(elem.id == "quick-reports"){
+	  	 	curr_child_num =1;
 		}
+	  	else if (elem.id == "my-folders"){
+	  	 	curr_child_num =2;
+	  	}
+	  	else if (elem.id == "my-team-folders"){ 
+	  		curr_child_num =3;
+	  	}
+	  	else if (elem.id == "public-folders"){ 
+	  		curr_child_num =4;
+	  	}
+		// var list_itemObj = { list_item: li};
+		
+			if(elem.id == "quick-reports" || elem.id == "my-folders" ||
+			elem.id == "my-team-folders" || elem.id == "public-folders"){
+			    // OnHashAux(tabs,prev_tab,elem.id,1,li);
+			    setTabStyle(prev_tab, {'display':'none', 'background':'rgb(80, 80, 80)'});
+				if(child_num != 0){
+				    li = tabs.querySelector("ul a:nth-child("+child_num+")");
+				    if(li!=null){
+					    li.style.background = "rgb(80, 80, 80)";
+					    li.style.color =  "white";
+					}
+				}
+				li = tabs.querySelector("ul a:nth-child("+curr_child_num+")");
+	 			if(li!=null){
+					li.style.color =  "black";
+					li.style.background = "rgb(235, 235, 235)";
+				}
+				setTabStyle(elem.id , {'display':'block', 'background':'rgb(235, 235, 235)'});
+				Initialize();		
+			}
 
-	elem.style.height = "740px";	
+		elem.style.height = "740px";	
 	}
 	
 
